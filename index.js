36ff86cc4537dcb669c153a919ea8e8ebf8c65e1
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-31 16:15:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let num1=31;
 let num2=2;
 
 let multiply= num1*num2;
-let random=math.ceil(math.random()*10)+1;
+let random=Math.ceil(Math.random()*10)+1;
 let mod=40%6;
-let max=math.max(1,19,17,20);
+let max=Math.max(1,19,17,20);
 
